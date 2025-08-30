6ac18fcf482132a6639a322baae309da52c5563a
Sreenath <sreenathsrp256@gmail.com>
2025-08-30 16:12:16 +0530

    Date change function added in the todays tasks

--- routes/subTaskRoutes.js
+++ routes/subTaskRoutes.js
@@ -2,29 +2,29 @@ import subTaskControllers from '../controllers/subTaskControllers.js';
 import express from 'express'
 import permissionMiddleware from '../middlewares/permissionMiddleware.js';
 
-const subTaskRoutes = ()=>{
+const subTaskRoutes = () => {
     const router = express.Router();
     const controllers = subTaskControllers()
 
-    router.post('/addSubTask',controllers.addSubTask)
-    router.patch('/updateName/:projectId',permissionMiddleware("task"),controllers.updateSubTaskName)
-    router.patch('/updateNote/:projectId',permissionMiddleware("notes"),controllers.updateSubTaskNote)
-    router.patch('/updateStatus/:projectId',controllers.updateSubTaskStatus)
+    router.post('/addSubTask', controllers.addSubTask)
+    router.patch('/updateName/:projectId', permissionMiddleware("task"), controllers.updateSubTaskName)
+    router.patch('/updateNote/:projectId', permissionMiddleware("notes"), controllers.updateSubTaskNote)
+    router.patch('/updateStatus/:projectId', controllers.updateSubTaskStatus)
 
 
-    router.patch('/updateClient/:projectId',permissionMiddleware("client"),controllers.updateSubTaskClient) 
+    router.patch('/updateClient/:projectId', permissionMiddleware("client"), controllers.updateSubTaskClient)
 
 
-    router.patch('/updatePriority/:projectId',controllers.updateSubTaskPriority)
-    router.patch('/updateDueDate/:projectId',permissionMiddleware("dueDate"),controllers.updateDueDate)
-    router.patch('/updateField/:projectId',permissionMiddleware("dynamic"),controllers.updateDynamicField)
-    router.patch('/assignSubTask/:projectId',permissionMiddleware("people"),controllers.assignSubTask)
-    router.patch('/removeSubTask/:projectId',permissionMiddleware("remove"),controllers.removeSubTsk)
-    router.patch('/dndSubTasks',controllers.dndSubTaskUpdate)
+    router.patch('/updatePriority/:projectId', controllers.updateSubTaskPriority)
+    router.patch('/updateDueDate/:projectId', controllers.updateDueDate)
+    router.patch('/updateField/:projectId', permissionMiddleware("dynamic"), controllers.updateDynamicField)
+    router.patch('/assignSubTask/:projectId', permissionMiddleware("people"), controllers.assignSubTask)
+    router.patch('/removeSubTask/:projectId', permissionMiddleware("remove"), controllers.removeSubTsk)
+    router.patch('/dndSubTasks', controllers.dndSubTaskUpdate)
 
 
-    router.get('/client',controllers.getClientSubtasks)
-    
+    router.get('/client', controllers.getClientSubtasks)
+
     return router
 }
 
